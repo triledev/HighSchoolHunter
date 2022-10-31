da326c3ce5b6317605d25d9b1898426452c24698
Tri Le <tri.le.mail@gmail.com>
2022-10-31 15:36:13 -0500

    Initial Commit

---
+++ HighSchoolHunter/HighSchoolHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighSchoolHunterApp.swift
+//  HighSchoolHunter
+//
+//  Created by Tri Le on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct HighSchoolHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
